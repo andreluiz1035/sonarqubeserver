b1c55a1fb26dad989a969fd38b9e9bc601fe8239
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-02-20 15:49:38 -0300

    Adicionando 3codesmell.py para teste Sonar

--- src/app/code_smell.py
+++ src/app/code_smell.py
@@ -1,6 +1,88 @@
-def soma1(a, b):
-    return a + b
+def processar_dados1(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
 
-def soma2(a, b):
-    return a + b
 
+def processar_dados2(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados3(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados4(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados5(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados6(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados7(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados8(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados9(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
+
+
+def processar_dados10(a, b):
+    resultado = a + b
+    if resultado > 10:
+        print("Maior que 10")
+    else:
+        print("Menor ou igual a 10")
+    return resultado
