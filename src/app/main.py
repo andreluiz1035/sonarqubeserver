4a23e0a70896dc3adcc9bb7072554eb05691ed8f
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-02-20 17:43:41 -0300

    Adicionando nova função com teste

--- src/app/main.py
+++ src/app/main.py
@@ -4,3 +4,8 @@ def soma(a, b):
 
 if __name__ == "__main__":
     print("Resultado:", soma(2, 3))
+
+
+def multiplica(a, b):
+    return a * b
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,3 +2,9 @@ from src.app.main import soma
 
 def test_soma():
     assert soma(2, 3) == 5
+
+
+from src.app.main import multiplica
+
+def test_multiplica():
+    assert multiplica(2, 3) == 6
